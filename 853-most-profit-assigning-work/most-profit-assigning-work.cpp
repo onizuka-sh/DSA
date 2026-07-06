2d4bb6ee5d152d6bbe73a054b940ff6f2ca51d53
Gaurav K. <142656078+onizuka-sh@users.noreply.github.com>
2026-07-06 23:04:51 +0530

    Time: 26 ms (28.81%) | Memory: 45 MB (17.32%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -3,28 +3,17 @@ public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
         int n = profit.size();
         int m = worker.size();
-        vector<pair<int, int>> vec (n);
+        priority_queue<pair<int, int>> pq;
         for (int i = 0; i<n; i++) {
-            vec[i] = {difficulty[i], profit[i]};
+            pq.push({profit[i], difficulty[i]});
         }
-        sort(vec.begin(), vec.end());
-        for (int i = 1; i<n; i++) {
-            vec[i].second = max(vec[i].second, vec[i-1].second);
-        }
-        int totalProfit = 0;
+        int result = 0;
+        sort(worker.rbegin(), worker.rend());
         for (int i = 0; i<m; i++) {
-            int l = 0, r = vec.size()-1;
-            int currProfit = 0;
-            while (l<=r) {
-                int mid = l+(r-l)/2;
-                if (worker[i] >= vec[mid].first) {
-                    l = mid+1;
-                    currProfit = vec[mid].second;
-                }
-                else r = mid-1;
-            }
-            totalProfit += currProfit;
+            while (!pq.empty() && worker[i] < pq.top().second) pq.pop();
+            if (pq.empty()) break;
+            result += pq.top().first;
         }
-        return totalProfit;
+        return result;
     }
 };
